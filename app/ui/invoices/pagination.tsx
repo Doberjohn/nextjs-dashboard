0022ddf5d40a3093e0d39a5515028d429cc11ce4
johnn <johnf@risk.nl>
2023-11-16 13:01:53 +0200

    Chapter 11/16 completed

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -31,7 +31,7 @@ export default function Pagination({totalPages}: { totalPages: number }) {
                {allPages.map((page, index) => {
                   let position: 'first' | 'last' | 'single' | 'middle';
 
-                  if (index === 0) position = 'first';
+                  position = 'first';
                   if (index === allPages.length - 1) position = 'last';
                   if (allPages.length === 1) position = 'single';
                   if (page === '...') position = 'middle';

50c3474aa74c607ae8f5259a7a1953cb730f1eaf
johnn <johnf@risk.nl>
2023-11-20 17:33:57 +0200

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import {inter} from '@/app/ui/fonts';
+import { Analytics } from '@vercel/analytics/react';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+export default function RootLayout({children}: {
+   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+   return (
+      <html lang="en">
+      <head>
+         <title>Next.js</title>
+      </head>
+      <body className={`${inter.className} antialiased`}>
+         {children}
+         <Analytics />
+      </body>
+      </html>
+   );
 }

bc32b6f2632f90ff4488a8852f9a0ffbe04a7896
johnn <johnf@risk.nl>
2023-11-22 19:15:38 +0200

    Refactored login redirects

--- auth.config.ts
+++ auth.config.ts
@@ -10,7 +10,7 @@ export const authConfig = {
          const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
          if (isOnDashboard) {
             if (isLoggedIn) return true;
-            return Response.redirect(new URL('/dashboard', nextUrl));
+            return Response.redirect(new URL('/login', nextUrl));
          } else if (isLoggedIn) {
             return Response.redirect(new URL('/dashboard', nextUrl));
          }
